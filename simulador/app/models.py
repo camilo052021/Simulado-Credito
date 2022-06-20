3f84e6da152df184fff7041b728528863093ef6c
camilo <camand5@gmail.com>
2022-06-20 08:39:26 -0500

    creacion del modelo pruebas usuario

--- simulador/app/models.py
+++ simulador/app/models.py
@@ -1,3 +1,17 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class PruebaUser(models.Model):
+    usuario_pruebas = models.CharField(max_length=30,verbose_name="Usuario Pruebas")
+    nombre_pruebas = models.CharField(max_length=30,verbose_name="Usuario Pruebas")
+    create_at = models.DateField(auto_now_add=True, verbose_name="Creado el", null=True)
+    modify_at = models.DateField(auto_now=True, verbose_name="Actualizado el")
+
+    class Meta:
+        verbose_name = 'Usuario Prueba'
+        verbose_name_plural = 'Usuarios Pruebas'
+
+    def __str__(self):
+        return f'Ususario Prueba {self.usuario_pruebas}'
